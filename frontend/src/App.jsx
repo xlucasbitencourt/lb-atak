9d849775df56663fcae3a908f4482d20c3bec325
Lucas Bitencourt <lblukz@gmail.com>
2023-07-06 21:39:24 -0300

    implementa bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./App.css";
 
 function App() {
   const [query, setQuery] = useState("");
@@ -8,6 +14,7 @@ function App() {
 
   const handleSearch = async (e) => {
     e.preventDefault();
+    if (!query) return;
     setResults([]);
     setLoading(true);
     try {
@@ -17,28 +24,56 @@ function App() {
       setResults(response.data);
     } catch (error) {
       console.error("Aconteceu um erro: ", error);
+      
     }
     setLoading(false);
   };
 
   return (
-    <div>
-      <h1>Lucas Bitencourt</h1>
-      <h3>Atak Sistemas - Teste para desenvolvimento</h3>
-      <form onSubmit={handleSearch}>
-        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
-        <button type="submit">Pesquisar</button>
-      </form>
+    <Container>
+      <div className="title">
+        <h1>Lucas Bitencourt</h1>
+        <h3>Atak Sistemas - Teste para desenvolvimento</h3>
+      </div>
+      <Form onSubmit={handleSearch}>
+        <Form.Group className="mb-3">
+          <Form.Label>Digite abaixo sua busca:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Digite sua busca"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Button className="search" variant="success" type="submit">
+            Pesquisar
+          </Button>
+          <Button
+            variant="danger"
+            type="reset"
+            onClick={() => (setQuery(""), setResults([]))}
+          >
+            Limpar
+          </Button>
+        </Form.Group>
+      </Form>
       {loading && <h2>Carregando...</h2>}
       <div>
         {results.map((result, index) => (
-          <div key={index}>
-            <h3>{result.title}</h3>
-            <a href={result.link} target="_blank" rel="noreferrer">{`[${result.link}]`}</a>
-          </div>
+          <Card style={{ margin: "1% 0" }} key={index}>
+            <Card.Body>
+              <Card.Text>{result.title}</Card.Text>
+              <Card.Link
+                href={result.link}
+                target="_blank"
+                rel="noreferrer"
+              >{`[${result.link}]`}</Card.Link>
+            </Card.Body>
+          </Card>
         ))}
       </div>
-    </div>
+    </Container>
   );
 }
 
